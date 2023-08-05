5e5a60b972db312df1c00a47323c6550d7197869
DanialFarros <danialfarrosmaulana921@gmail.com>
2023-08-05 08:15:51 +0700

    update frontend till opening comming soon

---
+++ resources/views/landingpage/slicing/opening-countdown.blade.php
@@ -0,0 +1,19 @@
+<div class="opening">
+    <div class="compe-bg">
+        <img class="ornamen1" src="{{ asset('civex/img/icon/opening-ornamen1.svg') }}" alt="">
+        <img class="ornamen2" src="{{ asset('civex/img/icon/opening-ornamen2.svg') }}" alt="">
+    </div>
+    <div class="container d-flex align-items-center justify-content-center">
+        <div>
+            <h1 class="text-center">SEK REG AKU ADA KONDANGAN</h1>
+            <p class="text-center">NTAR MALEM ABIS SOD TAK PUS</p>
+        </div>
+
+
+    </div>
+</div>
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::get('/', function () {$active = 'home'; return view('landingpage.home', c
 Route::get('/detail', function () {$active = 'home'; return view('landingpage.detail-civex', compact('active'));});
 Route::get('/competition', function () {$active = 'competition'; return view('landingpage.competition', compact('active'));});
 Route::get('/competition-cpcc', function () {$active = 'competition';return view('landingpage.competition-cpcc', compact('active'));});
+Route::get('/competition-gec', function () {$active = 'competition';return view('landingpage.competition-gec', compact('active'));});
+Route::get('/competition-cesc', function () {$active = 'competition';return view('landingpage.competition-cesc', compact('active'));});
+Route::get('/opening', function () {$active = 'opening';return view('landingpage.opening', compact('active'));});
 
 
 // Route::get('/coba', function () {return view('authentication.forgetpassword ');});
