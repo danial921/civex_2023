8b11e9608931f47d6a1d5701919c3822c21a1611
DanialFarros <danialfarrosmaulana921@gmail.com>
2023-08-24 04:07:18 +0700

    fe webinar

---
+++ resources/views/landingpage/slicing/event_sponsorship.blade.php
@@ -0,0 +1,43 @@
+<div class="footer-event">
+    <div class="copyright-bg" style="background: linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 9.65%);">
+        <img class="ornamen1" src="{{ asset('civex/img/sponsorship/bullets-red.svg') }}" alt="">
+        <img class="ornamen2" src="{{ asset('civex/img/sponsorship/bullets-yellow.svg') }}" alt="">
+        <img class="ornamen3" src="{{ asset('civex/img/sponsorship/bullets-blue.svg') }}" alt="">
+    </div>
+
+    <div class="footer-sponsor">
+        <div class="pt-lg-4 pt-2 pb-lg-4 pb-2">
+            <p class="title">OUR SPONSORSHIP</p>
+        </div>
+        <div class="container">
+            <div class="carousel-sponsor">
+                <article>
+                    <div>
+                        <ul class="m-0">
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_1.png') }}" /></li>
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_2.png') }}" /></li>
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_3.png') }}" /></li>
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_4.png') }}" /></li>
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_5.png') }}" /></li>
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_6.png') }}" /></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_1.png') }}" /></li>
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_2.png') }}" /></li>
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_3.png') }}" /></li>
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_4.png') }}" /></li>
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_5.png') }}" /></li>
+                            <li><img src="{{ asset('civex/img/sponsorship/sponsor_6.png') }}" /></li>
+                        </ul>
+                    </div>
+                </article>
+            </div>
+
+        </div>
+
+    </div>
+
+    @include('landingpage.slicing.event_copyright')
+</div>
---
+++ resources/views/landingpage/slicing/webinar-speaker.blade.php
@@ -0,0 +1,41 @@
+<section class="webinar-speaker">
+    <img class="headset" src="{{ asset('civex/img/icon/webinar_headset.svg') }}">
+    <img class="scope" src={{ asset('civex/img/icon/webinar_scope.svg') }}>
+    <img class="ex1" src="{{ asset('civex/img/icon/webinar_ex-1.svg') }}">
+    <img class="ex" src="{{ asset('civex/img/icon/webinar_ex.svg') }}">
+    <div class="title" data-aos="fade-in">Meet Our Speakers</div>
+    <div class="gallery js-flickity container" data-aos="fade-out" data-flickity-options='{ "wrapAround": true  }'>
+        <div class="gallery-cell img1"></div>
+        <div class="gallery-cell imgx"></div>
+        <div class="gallery-cell imgx"></div>
+        <div class="gallery-cell imgx"></div>
+        <div class="gallery-cell imgx"></div>
+    </div>
+    <div class="container">
+        <div class="information">
+        <div class="row m-0">
+            <img class="alien1 d-lg-block d-none" src="{{ asset('civex/img/icon/webinar_alienleft.svg') }}">
+            <img class="alien2 d-lg-block d-none" src="{{ asset('civex/img/icon/webinar_alienright.svg') }}">
+            <div class="col-4 date">
+                <div class="heading">DATE :</div>
+                <div class="content">October 15, 2022</div>
+            </div>
+            <div class="col-4 time">
+                <div class="heading">TIME :</div>
+                <div class="content">1 pm - End</div>
+            </div>
+            <div class="col-4 location">
+                <div class="heading">LOCATION :</div>
+                <div class="content">Zoom & Youtube</div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="cta container">
+            <div class="d-flex justify-content-center">
+                <a class="registerbtn" href=""><p>Register</p></a>
+                <a class="cpbtn ml-5" href="" ><p>Contact Person</p></a>
+            </div>
+        </div>
+    </div>
+</section>
