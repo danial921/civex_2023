d7df6750f82f84d16af2c514a1479cadcc101da6
Aushaf Amrega <rtpamrega@gmail.com>
2023-10-16 09:08:37 +0700

    ubah link url yt video tutroal

--- resources/views/cesc/dashboard.blade.php
+++ resources/views/cesc/dashboard.blade.php
@@ -19,7 +19,7 @@
                     <div class="card-body">
                         <p class="title">Video Tutorial Pendaftaran</p>
                         <!-- <iframe width="100%" height="400px" src="https://www.youtube.com/embed/ZM9RSg-ppOk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
-                            <!-- <iframe width="100%" height="400px" src="https://www.youtube.com/embed/ZM9RSg-ppOk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
+                            <iframe width="100%" height="400px" src="https://www.youtube.com/watch?v=omvCDkyNTOA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
--- resources/views/cesc/slicing/penyisihan-hero.blade.php
+++ resources/views/cesc/slicing/penyisihan-hero.blade.php
@@ -12,7 +12,7 @@
                     </button>
                 </a>
                 {{-- Tunggu Release --}}
-                @if(strtotime(date("Y-m-d H:i:s")) >= strtotime('2023-10-24 23:59:59'))
+                @if(strtotime(date("Y-m-d H:i:s")) >= strtotime('2023-9-24 23:59:59'))
                 <div class="mb-2"></div>
                 <a href="https://drive.google.com/file/d/10FssaXM7nFjx2eTLNmfTaAxPuExk64fw/view?usp=drivesdk" target="_blank">
                     <button type="button" class="btn btn-daftar  w-100">
@@ -38,7 +38,7 @@
                 <table class="transparent-table hero-desc" border="0">
                     <tr>
                         <th class="pr-3">Link </th>
-                        <td><a href="https://www.youtube.com/watch?v=omvCDkyNTOA">: {{ ENV('URL_CBT') }}</a></td>
+                        <td><a href="">: {{ ENV('URL_CBT') }}</a></td>
                       </tr>
                       <tr>
                         <th class="pr-3">Username</th>
