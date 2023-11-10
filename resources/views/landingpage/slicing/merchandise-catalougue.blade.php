30338a8d9f693eaf2059ba2ebba4045b761a1eb0
DanialFarros <danialfarrosmaulana921@gmail.com>
2023-11-10 22:54:06 +0700

    update

--- resources/views/landingpage/slicing/merchandise-catalougue.blade.php
+++ resources/views/landingpage/slicing/merchandise-catalougue.blade.php
@@ -1,30 +1,41 @@
-<div class="merch-katalog p-3">
-    <h1 class="mt-lg-5 mt-2">Silahkan Pilih Kategori:</h1>
-    <div class="mt-5">
+<div class="merch-katalog p-2 pl-5 pr-5">
+    <h1 class="mt-lg-3 mt-2">Silahkan Pilih Kategori:</h1>
+    <div class="mt-4 pb-lg-3">
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-2 d-flex flex-column">
+                <a class="btn btn-primary" onclick="showCategory('tshirt')"><span>T-Shirt</span></a>
+                <a class="btn btn-primary" onclick="showCategory('totebag')"><span>Totebag</span></a>
+                <a class="btn btn-primary" onclick="showCategory('sticker')"><span>Sticker</span></a>
+                <a class="btn btn-primary" onclick="showCategory('lighter')"><span>Lighter</span></a>
+                <a class="btn btn-primary" onclick="showCategory('keychain')"><span>Keychain</span></a>
+                <a class="btn btn-primary" onclick="showCategory('bundle')"><span>Bundle</span></a>
+                <a class="btn btn-primary" onclick="showSizeChart()"><span>Size Chart</span></a>
+            </div>
 
-
-                <div class=" mt-4" role="group" aria-label="Katalog Categories">
-                    <button class="btn btn-primary" onclick="showCategory('tshirt')">T-Shirt</button>
-                    <button class="btn btn-primary" onclick="showCategory('totebag')">Totebag</button>
-                    <button class="btn btn-primary" onclick="showCategory('sticker')">Sticker</button>
-                    <button class="btn btn-primary" onclick="showCategory('lighter')">Lighter</button>
-                    <button class="btn btn-primary" onclick="showCategory('keychain')">Keychain</button>
-                    <button class="btn btn-primary" onclick="showCategory('bundle')">Bundle</button>
-                    <button class="btn btn-primary" onclick="showSizeChart()">Size Chart</button>
+            <div class="col-md-10 p-3" id="gallery">
+                <div id="tshirt" class="gallery-category" style="display: block">
+                    <div class="row">
+                        <div class="col-3">
+                            <div class="item-card">
+                                <div class="image">
+                                    <img src="{{ asset('civex/img/katalog/t-shirt-white.png') }}" alt="">
+                                </div>
+                                <div class="button">
+                                    <img src="{{ asset('civex/img/katalog/detail-black.svg') }}" alt="">
+                                    <img src="{{ asset('civex/img/katalog/buy-now-black.svg') }}" alt="">
+                                </div>
+                                <div class="name">
+                                    TS-PT
+                                    Rp85.000
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-
-        <div class="row mt-5" id="gallery">
-            <div id="tshirt" class="gallery-category" style="display: block">
-                <img src="path/to/tshirt1.jpg" alt="T-Shirt 1">
-                <img src="path/to/tshirt2.jpg" alt="T-Shirt 2">
-            </div>
-            <div id="totebag" class="gallery-category" style="display: none">
-                <img src="path/to/totebag1.jpg" alt="Totebag 1">
-                <img src="path/to/totebag2.jpg" alt="Totebag 2">
+                <div id="totebag" class="gallery-category" style="display: none">
+                    <img src="path/to/totebag1.jpg" alt="Totebag 1">
+                    <img src="path/to/totebag2.jpg" alt="Totebag 2">
+                </div>
             </div>
         </div>
     </div>
