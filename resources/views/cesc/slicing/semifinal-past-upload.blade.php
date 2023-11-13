201f872ac013de2e2fa02c5c179e5ee755a3b89b
DanialFarros <danialfarrosmaulana921@gmail.com>
2023-11-13 16:37:55 +0700

    update link

--- resources/views/cesc/slicing/semifinal-past-upload.blade.php
+++ resources/views/cesc/slicing/semifinal-past-upload.blade.php
@@ -11,25 +11,34 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6 col-12">
-                <div class="mb-0 bd-highlight title">Rilis <span class="bold-blue">Kisi Kisi Semifinal</div>
-                <div class="mb-1 bd-highligt sub-title">download kisi kisi pada url berikut:</div>
-                <a href="submission-final">
+            <div class="col-lg-6 col-12 mb-2">
+                <div class="mb-lg-0 mb-0 bd-highlight title">Rilis <span class="bold-blue">Kisi Kisi Semifinal</div>
+                <div class="mb-0 bd-highligt sub-title">download kisi kisi pada url berikut:</div>
+                <a href="https://docs.google.com/document/d/1OX1unA5bUZfhlBfjDpMHymjgb68pXfLj90eJ91aE9M8/edit?usp=sharing">
                     <button type="button" class="btn btn-daftar w-100">
                         <img src="{{ asset('civex/img/icon/icon-paper.svg') }}" alt="your_image" width="20"
                             height="20">
                         <b>Kisi Kisi</b> Semifinal
                     </button>
                 </a>
-                <div class="mb-0 bd-highlight title">Rilis <span class="bold-blue">Soal Semifinal</div>
-                <div class="mb-1 bd-highligt sub-title">download soal pada url berikut:</div>
-                <a href="submission-final">
+                <div class="mt-lg-2 mt-1 mb-0 bd-highlight title">Rilis <span class="bold-blue">Guidebook</div>
+                <div class="mb-0 bd-highligt sub-title">guidebook pada url berikut:</div>
+                <a href="https://docs.google.com/document/d/1vTLeWx4P2FIdsCn4Vw2bLOWoIUEmN1lxVMfdCkjH20Q/edit?usp=sharing">
                     <button type="button" class="btn btn-daftar w-100">
                         <img src="{{ asset('civex/img/icon/icon-paper.svg') }}" alt="your_image" width="20"
                             height="20">
-                        <b>Soal</b> Semifinal
+                        <b>Guidebook</b> Semifinal
                     </button>
                 </a>
+                <div class="mt-lg-2 mt-1 mb-0 bd-highlight title"><span class="bold-blue">WhatsApp Group</div>
+                    <div class="mb-0 bd-highligt sub-title">bergabung group wa pada url berikut:</div>
+                    <a href="https://chat.whatsapp.com/CV7MFGQVHcTJsXL7bnZCEX">
+                        <button type="button" class="btn btn-daftar w-100">
+                            <img src="{{ asset('civex/img/icon/icon-wa.svg') }}" alt="your_image" width="20"
+                                height="20">
+                            <b>Group</b> Semifinal
+                        </button>
+                    </a>
             </div>
         </div>
     </div>
