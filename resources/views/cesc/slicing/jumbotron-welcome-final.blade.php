c621bd53f908e7f20d18846b0c9bcd594c70e7a0
Aushaf Amrega <rtpamrega@gmail.com>
2023-11-28 12:01:19 +0700

    add pdf to final cesc

--- resources/views/cesc/slicing/jumbotron-welcome-final.blade.php
+++ resources/views/cesc/slicing/jumbotron-welcome-final.blade.php
@@ -8,16 +8,25 @@
                 <div class="d-flex align-items-end h-100">
                     <div class="">
                         <div class="pb-1 bd-highlight title">One Step Closer,  <span class="bold-blue">{{ $username }}</div>
-                        <div class="pb-1 bd-highligt sub-title">Tim Kamu Menjadi Finalis CPCC 2023</div>
+                        <div class="pb-1 bd-highligt sub-title">Tim Kamu Menjadi Finalis CESC 2023</div>
                         <div class="hint-blue pb-2">Silahkan Begabung kedalam WA Grup Finalis pada link berikut</div>
-                        <div class="col-lg-10 col-12">
-                            <a href="submission-final">
-                                <button type="button" class="btn btn-daftar w-100">
-                                    <img src="{{ asset('civex/img/icon/icon-wa.svg') }}" alt="your_image"
-                                        width="20" height="20">
-                                        Link <b>WA Group</b> Finalis CPCC 2023
-                                </button>
-                            </a>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <a href="submission-final">
+                                    <button type="button" class="btn btn-daftar w-100">
+                                        <img src="{{ asset('civex/img/icon/icon-wa.svg') }}" alt="your_image"
+                                            width="20" height="20">
+                                            Link <b>WA Group</b> Finalis  2023
+                                    </button>
+                                </a>
+                            </div>
+                            <div class="col-lg-6">
+                                <a href="https://drive.google.com/file/d/1RTe0LjmQYFW8ljKi34tEl-my0HHD53Y9/view" target="_blank">
+                                    <button type="button" class="btn btn-daftar w-100">
+                                        <b>Surat Keputusan</b> Finalis CESC 2023
+                                    </button>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
